7ba5466bbd85ede5e708bf587bba653f018c2d86
Arthur Sakayan Vieira de Melo <asvdem@gmail.com>
2023-12-09 14:28:30 -0300

    Add some tests to the main file

--- C source code/main.c
+++ C source code/main.c
@@ -1,4 +1,20 @@
-int main (/*int argc, char *argv[]*/) {
-	//TODO: Showcase dynamic forest functionality
+#include <stdlib.h>
+#include <stdio.h>
+#include "dynamicForest.h"
+
+int main (int argc, char *argv[]) {
+	int n;
+	if (argc == 2)
+		n = atoi (argv[1]);
+	else {
+		puts ("Please type the number of nodes that you want in your dynamic forest.");
+		printf ("You can also pass this on the command line, like this: %s <number of nodes>\n", argv[0]);
+		char numberBuffer[10];
+		if (fgets (numberBuffer, sizeof (numberBuffer), stdin) == NULL){
+			perror ("fgets error");
+		}
+		n = atoi (numberBuffer);
+	}
+	Node *dynamicForest = makeDynamicForest (n);
 	return 0;
 }
