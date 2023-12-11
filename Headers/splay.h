b6d44bc89d315bbefaadf384ee4e56bc69ce5b62
Arthur Sakayan Vieira de Melo <asvdem@gmail.com>
2023-12-11 17:56:32 -0300

    Reorder functions on the splay header
    After all, it makes more sense for the function that creates the splay
    tree to be at the very top.

--- Headers/splay.h
+++ Headers/splay.h
@@ -1,8 +1,8 @@
 #ifndef __SPLAY_H__
 #define __SPLAY_H__
 #include "node.h"
-void joinSplay (Node v, Node w);
 void createSplay (Node x);
+void joinSplay (Node v, Node w);
 void reflectSplay (Node v);
 
 #endif /* __SPLAY_H__ */
